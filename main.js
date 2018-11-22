d3dd4b7d590d98fabb7b53d8c314c86184da1641
Aadish Surana <aadish.surana@qiksolve.com>
2018-11-22 12:02:28 +1100

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+import style from './main.css';
+
+const appName = 'Webpack Demo';
+setTimeout(
+  () => alert(`Thanks for using ${appName}`)
+, 500);
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    entry: './main.js',
+    output: {
+        path: __dirname,
+        filename: './dist/build.js'
+    },
+    watch: true,
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    { loader: "style-loader" },
+                    { loader: "css-loader" }
+                ]
+            },
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env']
+                    }
+                }
+            }
+        ]
+    }
+};
